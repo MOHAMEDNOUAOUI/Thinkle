9c46eb317c327ef1ee9fc90f499a51ed7d321c53
MOHAMED <mohamednouaoui5@gmail.com>
2025-07-20 20:31:05 +0100

    first commit with file creation and loading up the layout

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ComponentLayout from './layouts/componentLayout'
+
+function App() {
+
+  return (
+    <>
+      <ComponentLayout />
+    </>
+  )
+}
+
+export default App
---
+++ src/layouts/componentLayout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ComponentLayout() {
+  return (
+    <div>Component Layout</div>
+  )
+}
+
+export default ComponentLayout
