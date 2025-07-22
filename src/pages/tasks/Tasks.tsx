3814fd57b283ad5dc284f3a177ac9b97a07c5740
Mohamed <mohamednouaoui5@gmail.com>
2025-07-22 17:04:46 +0100

    made some minor changes that i dont know about jhhh

--- src/pages/tasks/Tasks.tsx
+++ src/pages/tasks/Tasks.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
+// import React from 'react'
 import './tasks.scss'
 import ShinyButton from '../../components/ShinyButton/ShinyButton'
 import DesignSwitch from './components/tasks-design_switcher/Design-Switch'
-import { AnimatePresence, motion } from "framer-motion"
-import { useState } from "react"
+// import { AnimatePresence, motion } from "framer-motion"
+// import { useState } from "react"
 
 function Tasks() {
-  const [isVisible, setIsVisible] = useState(false)
+  // const [isVisible, setIsVisible] = useState(false)
 
 
   return (
@@ -47,28 +47,28 @@ function Tasks() {
 
 export default Tasks
 
-const container: React.CSSProperties = {
-    display: "flex",
-    flexDirection: "column",
-    width: 100,
-    height: 160,
-    position: "relative",
-}
+// const container: React.CSSProperties = {
+//     display: "flex",
+//     flexDirection: "column",
+//     width: 100,
+//     height: 160,
+//     position: "relative",
+// }
 
-const box: React.CSSProperties = {
-    width: 100,
-    height: 100,
-    backgroundColor: "#0cdcf7",
-    borderRadius: "10px",
-}
+// const box: React.CSSProperties = {
+//     width: 100,
+//     height: 100,
+//     backgroundColor: "#0cdcf7",
+//     borderRadius: "10px",
+// }
 
-const button: React.CSSProperties = {
-    backgroundColor: "#0cdcf7",
-    borderRadius: "10px",
-    padding: "10px 20px",
-    color: "#0f1115",
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    right: 0,
-}
+// const button: React.CSSProperties = {
+//     backgroundColor: "#0cdcf7",
+//     borderRadius: "10px",
+//     padding: "10px 20px",
+//     color: "#0f1115",
+//     position: "absolute",
+//     bottom: 0,
+//     left: 0,
+//     right: 0,
+// }
